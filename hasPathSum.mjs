c87fa4ce429dd74716620ff06ae07eb4f657fa2f
Vadim Hladchenko <vadimhl@gmail.com>
2021-02-04 16:34:09 +0200

    final hasPathSum

--- hasPathSum.mjs
+++ hasPathSum.mjs
@@ -5,7 +5,6 @@ var hasPathSum = function(root, targetSum) {
 	let rez = false; 
 	let PathSum = function( node, sum ) {
 	    sum += node.val;
-	    console.log( `val=${node.val}  sum=${sum}`)
 		if ( !node.left && !node.right ) {
 			if ( sum === targetSum ) rez = true;
 			return;
@@ -19,14 +18,9 @@ var hasPathSum = function(root, targetSum) {
 	}
 	PathSum(root, 0);
 	return rez;
-
 };
 
-let tree;
-//tree=new TreeNode( 2, null ,new TreeNode( 3, null, new TreeNode( 4, null, new TreeNode( 5, null, new TreeNode( 6, null, null)))));
-
-tree =  treeFromArr ([5,4,8,11,null,13,4,7,2,null,null,null,1]);
-//tree =  treeFromArr ([2,null,3,null,4,null,5,null,6]);
+let tree =  treeFromArr ([5,4,8,11,null,13,4,7,2,null,null,null,1]);
 
 logTree( tree );
 console.log(hasPathSum(tree, 22))
