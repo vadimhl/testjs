6c9dd2593abc7b4105ee40d7a0076c5756a81197
Vadim Hladchenko <vadimhl@gmail.com>
2021-02-05 15:29:06 +0200

    comm

---
+++ maxProfit.js
@@ -0,0 +1,17 @@
+//121. Best Time to Buy and Sell Stock
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProf = 0; 
+    if (prices.length<2) return 0;
+    let minBuy = prices[0];
+    for ( let i = 1; i<prices.length; i++ ) {
+        let cur = prices[i] ;
+        maxProf = Math.max(cur - minBuy, maxProf);
+        minBuy = Math.min(cur, minBuy);
+    }
+    return maxProf;    
+};
+console.log(maxProfit( [7,2,5,3,1,3] ) );
