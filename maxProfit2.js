c36907f2a8d8235a5ea8d8cc6d0a764f8c7bf1a0
Vadim Hladchenko <vadimhl@gmail.com>
2021-02-09 15:56:05 +0200

    Best Time to Buy and Sell Stock II reduce

--- maxProfit2.js
+++ maxProfit2.js
@@ -3,17 +3,12 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
-    let maxProf = 0;
-    let curProf = 0;
-    for (let i = 1; i < prices.length; i++) {
-        if ( prices[i]>= prices[i-1]) {
-            curProf += prices[i] - prices[i-1];
-        } else {
-            maxProf += curProf;
-            curProf = 0;
-        }
-    }
-    return maxProf+curProf; 
-};
-console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+var maxProfit = function(prices) {
+    return prices.reduce( (sum, val, i, arr) => 
+                          {   if ( i==0)  return 0;
+                              return sum + (val>arr[i-1] ? val-arr[i-1] : 0)  
+                          }
+                         , 0);
+}
+console.log(maxProfit([7,1,5,3,6,4]));
+//console.log(`sum=${sum}  i=${i} add=${add}  val=${val} prices[i]=${prices[i]}`);
