07c1dd5fe0ea9db335488004f21430f1b405fdab
Vadim Hladchenko <vadimhl@gmail.com>
2021-02-11 10:22:51 +0200

    convertToTitl

--- convertToTitle.js
+++ convertToTitle.js
@@ -1,13 +1,12 @@
 var convertToTitle = function(n) {
     let rez='';
+    let codeA="A".codePointAt(0)-1;
     while ( n >= 1 ) {
         let ch = n % 26;
         if ( ch ==0 ) ch=26;
-        //
-        rez = String.fromCodePoint("A".codePointAt(0)-1+ch)+rez;
+        rez = String.fromCodePoint(codeA+ch)+rez;
         n = (n-ch)/26;
     }
-    //rez = String.fromCodePoint("A".codePointAt(0)-1+n)+rez;
     return rez;
 };
 console.log(convertToTitle(52));
