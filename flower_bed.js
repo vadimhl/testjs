cac115d4909dac3654b2e3b17a46b5ff4dc21010
Vadim Hladchenko <vadimhl@gmail.com>
2021-03-19 16:21:08 +0200

    flower_bed

--- flower_bed.js
+++ flower_bed.js
@@ -14,10 +14,15 @@ function outG (garden) {
     }
 N=9
 const vertex = [
-[2, 2],
-[7, 4],
-[4, 8],
-[1, 5],
+    [1, 5],
+    [3, 11],
+    [4, 8],
+    [5, 11],
+    [7, 8],
+    [6, 5],
+    [4, 8],
+    [7, 2],
+    [2, 2],
 ];
 const maxX=vertex.reduce( (pre, cur) => Math.max(pre, cur[0]), 0);
 const maxY=vertex.reduce( (pre, cur) => Math.max(pre, cur[1]), 0);
@@ -33,24 +38,51 @@ for (let vrt of vertex) {
     garden[vrt[0]][vrt[1]]='#';
 }
 outG(garden);
-const hors = new Array(maxY+1);
-const verts = new Array(maxX+1);
-hors.fill([]);
-verts.fill([]);
+const hors = [];
+const verts = [];
+for (let i=0; i<=maxY; i++) {
+    hors.push([]);
+}
+for (let i=0; i<=maxX; i++) {
+    verts.push([]);
+}
 vertex.push(vertex[0]);
 for (let n=1; n<vertex.length; n++) {
     let x0 = vertex[n-1][0];
     let y0 = vertex[n-1][1];
     let x1 = vertex[n][0];
     let y1 = vertex[n][1];
-    for ( let x = Math.min(x0, x1); x<=Math.max(x0, x1); x++) {
-        y = y0 + x0*(y1-y0)/(x1-x0);
-        //console.log(`n=${n} x=${x} x0=${x0} x1=${x1}`);
+    verts[x0].push(y0);
+    for ( let x = Math.min(x0, x1)+1; x<=Math.max(x0, x1)-1; x++) {
+        y = y0 + (x-x0)*(y1-y0)/(x1-x0);
+        //console.log(`n=${n} x=${x} y=${y}  x0=${x0} x1=${x1} y0=${y0} y1=${y1}`);    
         verts[x].push(y);
     }
-    for ( let y = Math.min(y0, y1); y<=Math.max(y0, y1); y++) {
-        x = x0 + y0*(x1-x0)/(y1-y0);
+    hors[y0].push(x0);
+    for ( let y = Math.min(y0, y1)+1; y<=Math.max(y0, y1)-1; y++) {
+        x = x0 + (y-y0)*(x1-x0)/(y1-y0);
         hors[y].push(x);
     }
 }
-console.log(verts);
+
+for (let x=0 ; x<verts.length; x++) {
+    console.log( x+' ---------------------------------------------' );
+    verts[x].sort( (a,b)=> a-b);
+    console.log( verts[x]);
+    if (verts[x].length>1) {
+        let cnt=0;
+        let xv=0;
+        for (let y=0; y<maxY; y++){
+            if ( y >=verts[x][xv] ) {
+                //console.log(`x=${x} y=${y} cnt=${cnt} xv=${xv} `);
+                cnt++;
+                xv++;
+            }
+            if ( cnt % 2 !==0 ) {
+                garden[x][y]='f';
+            }
+        }
+    }
+    
+}
+outG(garden);
